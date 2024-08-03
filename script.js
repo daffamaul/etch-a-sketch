73e818cf4d181f26d3d284e619fc849b5bfd0f13
daffamaul <dffmln2004@gmail.com>
2024-08-03 17:43:34 +0700

    Refator Function createSquares
    Adding new new function named squares, but little bug with rgb color when mouse is move

--- script.js
+++ script.js
@@ -2,29 +2,20 @@ const container = document.querySelector(".container");
 const changeSquares = document.querySelector("#squares");
 const erase = document.querySelector("#erase");
 
-const createSquares = function () {
-  let numberOfSquares = Number.parseInt(
-    prompt("Please input number of squares [1-100]: ")
-  );
-
-  if (numberOfSquares < 1 || numberOfSquares > 100)
-    return alert("Please input number [1-100]");
-
-  container.replaceChildren("");
-
-  for (let i = 1; i <= numberOfSquares; i++) {
+const squares = (total) => {
+  for (let i = 1; i <= total; i++) {
     const coll = document.createElement("div");
 
     coll.classList.add("coll");
-    coll.style.width = `${960 / numberOfSquares}px`;
+    coll.style.width = `${960 / total}px`;
     coll.style.border = "1px solid black";
 
-    for (let j = 1; j <= numberOfSquares; j++) {
+    for (let j = 1; j <= total; j++) {
       const cell = document.createElement("div");
 
       cell.classList.add("cell");
-      cell.style.width = `${960 / numberOfSquares}px`;
-      cell.style.height = `${960 / numberOfSquares}px`;
+      cell.style.width = `${960 / total}px`;
+      cell.style.height = `${960 / total}px`;
       cell.style.border = "1px solid black";
 
       coll.appendChild(cell);
@@ -47,5 +38,21 @@ const createSquares = function () {
     );
   });
 };
+
+const createSquares = function () {
+  let numberOfSquares = Number.parseInt(
+    prompt("Please input number of squares [1-100]: ")
+  );
+
+  if (numberOfSquares < 1 || numberOfSquares > 100)
+    return alert("Please input number [1-100]");
+
+  container.replaceChildren("");
+
+  squares(numberOfSquares);
+};
+
+// default squares
+squares(64);
 
 changeSquares.addEventListener("click", createSquares);
